4439cdae8830a44968b2e50088398c238d671afa
seif <seif1994@yahoo.fr>
2019-07-18 17:19:58 +0200

    integrate template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './shared/main-header/main-header.component';
+import { MainFooterComponent } from './shared/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,

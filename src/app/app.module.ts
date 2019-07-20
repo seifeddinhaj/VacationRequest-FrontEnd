0464976cd1ce8a023e68141d94b3eeb7e09c854f
seif <seif1994@yahoo.fr>
2019-07-20 09:52:19 +0200

    install Angular2TokenService and test the connection between the api and the back office

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './shared/main-header/main-header.component';
 import { MainFooterComponent } from './shared/main-footer/main-footer.component';
+import { Angular2TokenService } from 'angular2-token';
+import {  HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +17,9 @@ import { MainFooterComponent } from './shared/main-footer/main-footer.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpModule,
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
